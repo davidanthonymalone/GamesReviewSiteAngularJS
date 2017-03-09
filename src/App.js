a1cef4cd458a990353c0cab9c9a9ea916b31f626
David Anthony Malone <davidanthonymalone@gmail.com>
2017-03-09 14:41:15 +0000

    added in the css and a navbar to the application

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div classNameNameName="container">
+            <nav classNameName="navbar navbar-inverse navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">Game Review Site</a>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="navbar" className="collapse navbar-collapse">
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Home</a></li>
+            <li><a href="#about">Submit a Game Review</a></li>
+          </ul>
+        </div>
       </div>
+    </nav>
+        </div>
+     
     );
   }
 }
