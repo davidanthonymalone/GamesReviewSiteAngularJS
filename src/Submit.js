8f661b660f11142150d4754a9a45b4af2ecb5445
David Anthony Malone <davidanthonymalone@gmail.com>
2017-03-09 17:07:19 +0000

    testing pushing to heroku so need to commit

--- src/Submit.js
+++ src/Submit.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 
 class Submit extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {};
+        this.submitReview = this.submitReview.bind(this);
+    }
+    submitReview(){
+        console.log('Submit Review');
+    }
     render(){
         return(
             <div className="row">
             <div className="col-xs-12 col-sm12">
         <h1> <br/> Submit</h1>
            <form>
+            
   <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
+    <label htmlFor="Name">Name</label>
+    <input type="text" className="form-control" id="review" placeholder="Enter name of game you want to review?" />
   </div>
+            
   <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+    <label htmlFor="Review">Review</label>
+    <textarea className="form-control" id="review" placeholder="Leave your review for the game here!" />
+            
   </div>
-  
-  <div className="checkbox">
-    <label>
-      <input type="checkbox" /> Check me out
-    </label>
+            
+    <div className="form-group">
+    <label htmlFor="Category">Category</label>
+    <input type="text" className="form-control" id="Genre" placeholder="What Genre is the game?" />
+            
+            
+            
+   
   </div>
-  <button type="submit" className="btn btn-default">Submit</button>
+        
+     <div className="form-inilne form-group">
+    <label htmlFor="hashtag">Tag</label>
+    <input type="text" className="form-control" id="tag" placeholder="add a tag?" />
+              <button type="submit" className="btn btn-default">Add a Tag</button>
+
+  </div>
+        
+  <button type="submit" onClick={this.submitReview} className="btn btn-default">Submit</button>
 </form>
             </div>
             </div>
