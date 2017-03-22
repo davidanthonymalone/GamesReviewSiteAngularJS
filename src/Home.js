0afdf95d63ec9134afa82587005ea110a435cc4a
David Anthony Malone <davidanthonymalone@gmail.com>
2017-03-22 15:23:59 +0000

    i got search working in the react app

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+
+function searchingFor(term){
+    return function(x){
+        return x.name.toLowerCase().includes(term.toLowerCase()) || !term;
+    }
+}
+const reviews = {};
 class Home extends Component {
     
     constructor(props){
@@ -7,10 +14,19 @@ class Home extends Component {
         
         this.state = {
             
-            reviews: JSON.parse(localStorage.getItem('reviews')) || []};
+            reviews: JSON.parse(localStorage.getItem('reviews')) || [],
+            term: '',
+        
+        
+        };
+        
+        this.searchHandler = this.searchHandler.bind(this);
     }
     
-    
+    searchHandler(event){
+        this.setState({term: event.target.value})
+        
+    }
     
     
     	displayReviews() {
@@ -19,15 +35,24 @@ class Home extends Component {
             
 
           
-		this.state.reviews.map((review, i) => {
+		this.state.reviews.filter(searchingFor(this.state.term)).map((review, i) => {
+                
 			resultsArray.push(
 				<div className="col-sm-6 col-md-4">
+                
 					<div className="thumbnail">
-						
+						 <img src={review.image} alt={review.name}/>
+                             <div class="caption">
+                       
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-primary" role="button">Update</a> <a href="#" class="btn btn-default" role="button">Delete</a></p>
+                      </div>
 			        <h3>{review.name}</h3>
 			        <p>{review.review}</p>
 			      </div>
+                
 				</div>
+                
 				);
 		});
 
@@ -38,6 +63,10 @@ class Home extends Component {
             <div>
            
             <h1>  <br/> Home</h1>
+            <form>
+                <input type="text"
+                    onChange={this.searchHandler}/>
+                </form>
            {this.displayReviews()}
             </div>
         );
--- src/Submit.js
+++ src/Submit.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+const CLOUDINARY_UPLOAD_PRESET = 'rwvxi2lv';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dqajckyao/upload';
 
 
 class Submit extends Component {
@@ -13,10 +17,40 @@ class Submit extends Component {
                 name: "Name of Movie",
                 review: "Review",
                 
-            }
+            },
+            uploadedFileCloudinaryUrl: ''
         };
         this.submitReview = this.submitReview.bind(this);
+        this.onImageDrop = this.onImageDrop.bind(this);
     };
+    
+    onImageDrop(files){
+         this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+        
+    }
+    
+    
+    handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                        .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
     submitReview(){
         console.log('Submit Review');
         console.log(this.name.value, this.review.value);
@@ -24,6 +58,7 @@ class Submit extends Component {
         
         newReview.name = this.name.value;
         newReview.review = this.review.value;
+        newReview.image = this.state.uploadedFileCloudinaryUrl;
         
         this.setState({newReview});
         let reviews = this.state.reviews;
@@ -49,11 +84,25 @@ class Submit extends Component {
         <h1> <br/> Submit</h1>
             
             
- <form>          
+ <form>      
+            
+ <Dropzone
+      multiple={false}
+      accept="image/*"
+      onDrop={this.onImageDrop}>
+      <p>Drop an image or click to select a file to upload.</p>
+    </Dropzone>
+       <div>
+        {this.state.uploadedFileCloudinaryUrl === '' ? null :
+        <div>
+          <p>{this.state.uploadedFile.name}</p>
+          <img alt={this.state.uploadedFile.name} src={this.state.uploadedFileCloudinaryUrl} />
+        </div>}
+      </div>
   <div className="form-group">
     <label htmlFor="Name">Name</label>
     <input type="text" 
-    ref= {(input) => {this.name = input;}}
+    ref={(input) => {this.name=input;}}
     className="form-control" 
     id="name" 
     
@@ -63,7 +112,7 @@ class Submit extends Component {
   <div className="form-group">
     <label htmlFor="Review">Review</label>
     <textarea className="form-control"
-    ref= {(input) => {this.review = input;}}
+    ref={(input)=>{this.review = input;}}
     id="review" 
     placeholder="Leave your review for the game here!" />
             
